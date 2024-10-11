a22a55aea833d81d9b37dda357bc5db939ba1298
David Garcia <hi@davidgarcia.dev>
2024-10-11 12:38:57 +0100

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+
+## [1.0.1] - 2024-05-24
+
+- Introduced the `version` option, enabling specification of the version of the pushfeedback package to utilize from NPM.
+
+## [1.0.0] - 2024-02-08
+
+### Added
+
+- Compatibility with Docusaurus 3.x.
+
+## [0.1.7] - 024-02-08
+
+### Added
+
+- Open source release, compatible with Docusaurus 2.x.
---
+++ src/index.tsx
@@ -0,0 +1,93 @@
+interface FeedbackOptions {
+    feedbackButtonText?: string;
+    customFont?: string;
+    errorMessage?: string;
+    errorMessage403?: string;
+    errorMessage404?: string;
+    modalTitle?: string;
+    modalTitleSuccess?: string;
+    modalTitleError?: string;
+    modalPosition?: string;
+    sendButtonText?: string;
+    project?: string;
+    screenshotButtonText?: string;
+    screenshotTopbarText?: string;
+    email?: string;
+    emailPlaceholder?: string;
+    messagePlaceholder?: string;
+    buttonStyle?: string;
+    buttonPosition?: string;
+    hideIcon?: boolean;
+    hideScreenshotButton?: boolean;
+    version?: string;
+}
+
+const DEFAULT_OPTIONS: FeedbackOptions = {
+    feedbackButtonText: 'Feedback',
+    modalPosition: "bottom-right",
+    buttonStyle: "light",
+    buttonPosition: 'bottom-right',
+};
+
+function camelToKebab(string: string): string {
+    return string.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
+}
+
+function generateFeedbackScript(options: FeedbackOptions): string {
+    const mergedProps: FeedbackOptions = { ...DEFAULT_OPTIONS, ...options };
+    const { project, feedbackButtonText, ...feedbackProps } = mergedProps;
+    let scriptContent = `
+        const feedbackButton = document.createElement('feedback-button');
+        feedbackButton.setAttribute('project', '${project || ""}');
+    `;
+
+    Object.entries(feedbackProps).forEach(([key, value]) => {
+        if (value !== undefined) {
+            scriptContent += `
+                feedbackButton.setAttribute('${camelToKebab(key)}', '${value.toString()}');
+            `;
+        }
+    });
+
+    scriptContent += `
+        feedbackButton.textContent = '${feedbackButtonText || DEFAULT_OPTIONS.feedbackButtonText}';
+        document.body.appendChild(feedbackButton);
+    `;
+
+    return scriptContent;
+}
+
+module.exports = function (_context: any, options: FeedbackOptions) {
+
+    const version = options.version || 'latest'; 
+    return {
+        name: 'docusaurus-feedback-plugin',
+
+        injectHtmlTags() {
+            return {
+                headTags: [
+                    {
+                        tagName: 'link',
+                        attributes: {
+                            rel: 'stylesheet',
+                            href: `https://cdn.jsdelivr.net/npm/pushfeedback@${version}/dist/pushfeedback/pushfeedback.css`,
+                        },
+                    },
+                ],
+                postBodyTags: [
+                    {
+                        tagName: 'script',
+                        attributes: {
+                            type: 'module',
+                            src: `https://cdn.jsdelivr.net/npm/pushfeedback@${version}/dist/pushfeedback/pushfeedback.esm.js`,
+                        },
+                    },
+                    {
+                        tagName: 'script',
+                        innerHTML: generateFeedbackScript(options),
+                    }
+                ]
+            };
+        },
+    };
+};
